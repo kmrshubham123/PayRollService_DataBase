6d71e915a5ef1849ac954237dfa8c71c3c144775
kmrshubham123 <kmrshubham123@gmail.com>
2021-06-29 17:23:00 +0530

    [Kumar Shubham]Added:Insert Value for Terissa and update her details and also update details for remaining Employee

--- Payrollservice.sql
+++ Payrollservice.sql
@@ -51,7 +51,7 @@ Select SUM(Salary) from EmployeePayroll where Gender = 'Male' GROUP BY Gender;
 Alter Table EmployeePayroll Add
 PhoneNumber varchar(10),
 Department varchar(30),
-Adreess varchar(30), 
+Address varchar(30), 
 CONSTRAINT DF_Address DEFAULT 'Indian' FOR Address
 
 --UC9-Extend the EmployeePayroll table with Basic Pay,Deduction,Taxable Pay,Income Tax,Net Pay
@@ -61,4 +61,17 @@ Deduction float,
 TaxablePay float,
 IncomeTax float,
 NetPay float;
+
+
+--UC10-Insert Value for Terissa and update her details and also update details for remaining Employee
+Insert into EmployeePayroll(EmployeeName,Salary,StartDate)
+VALUES ('Terissa', 70000.00 , '2021-05-26')
+UPDATE EmployeePayroll set Gender ='Female' where EmployeeName='Terissa'
+UPDATE EmployeePayroll set PhoneNumber ='9888886897', Department='Sales & Marketing', Address='Pune',BasicPay=25000, Deduction=3000,TaxablePay=1000,IncomeTax=200,NetPay=17000 where EmployeeName='Kumar Shubham'
+UPDATE EmployeePayroll set PhoneNumber ='9888846788', Department='HR', Address='Delhi',BasicPay=15000, Deduction=4000,TaxablePay=1000,IncomeTax=400,NetPay=19000 where EmployeeName='Abhishek Kumar'
+UPDATE EmployeePayroll set PhoneNumber ='9900044556', Department='Support', Address='Gurgaon',BasicPay=18000, Deduction=5000,TaxablePay=1000,IncomeTax=400,NetPay=21000 where EmployeeName='Roshan Thakur'
+UPDATE EmployeePayroll set PhoneNumber ='9878787777', Department='Installation', Address='Kolkata',BasicPay=32000, Deduction=7000,TaxablePay=2000,IncomeTax=200,NetPay=18000 where EmployeeName='Vivek Verma'
+UPDATE EmployeePayroll set PhoneNumber ='9798989899', Department='HR', Address='Amritsar',BasicPay=30000, Deduction=5000,TaxablePay=1000,IncomeTax=200,NetPay=19000 where EmployeeName='Puja Raj'
+UPDATE EmployeePayroll set PhoneNumber ='9234567834', Department='Sales & Marketing', Address='Delhi',BasicPay=31000, Deduction=2000,TaxablePay=500,IncomeTax=300,NetPay=18000 where EmployeeName='Rekha'
+UPDATE EmployeePayroll set PhoneNumber ='7889809999', Department='Sales & Marketing', Address='Mumbai',BasicPay=35000, Deduction=2000,TaxablePay=1000,IncomeTax=200,NetPay=18000 where EmployeeName='Terissa'
 
