468a6914ecb1d9e3ab8cb533604302d7fe655b09
kmrshubham123 <kmrshubham123@gmail.com>
2021-06-29 15:04:17 +0530

    [Kumar Shubham]Added:Insert Values into EmployeePayRoll

--- Payrollservice.sql
+++ Payrollservice.sql
@@ -10,3 +10,11 @@ Salary float,
 StartDate date
 )
 
+--UC3-Ability to create Employee Payrolls Data 
+Insert into EmployeePayroll(EmployeeName,Salary,StartDate)
+VALUES ('Kumar Shubham', 50000.00 , '2021-06-28'),
+        ('Abhishek Kumar',55000.00, '2021-07-22'),
+		('Roshan Thakur',45000.00, '2021-08-26'),
+		('Vivek Verma',40000.00, '2021-09-25'),
+		('Puja Raj',42000.00, '2021-03-25'),
+		('Rekha',36000.00, '2021-02-15');
